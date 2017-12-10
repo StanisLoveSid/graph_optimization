68f9fd025704fc23a216e62eb017ef9386e39d75
StanisLoveSid <joingamings@gmail.com>
2017-12-10 11:50:17 +0200

    mod output

--- algorytm.rb
+++ algorytm.rb
@@ -1,51 +1,72 @@
 include Math
 
-def down_s
-  graph = { 1 => [], 2 => [1], 3 => [1], 4 => [2,3], 5 => [3,6], 6 => [] }
-  history = []
-  show = []
-  i = 0
-  until graph.empty?
-    i += 1
-    break puts "Цикличность недоступна" if i > graph.length
-    keys = []
-    graph.each do |key, value|
-      if value.empty?
-        history << key
-        keys << key
-        graph.delete key
+class AssocativeScores
+  def down_s(graph)
+    history = []
+    show = []
+    i = 0
+    until graph.empty?
+      i += 1
+      break puts "Цикличность недоступна" if i > (graph.length*4)
+      keys = []
+      graph.each do |key, value|
+        if value.empty?
+          history << key
+          keys << key
+          graph.delete key
+        end
       end
-    end
-    show << keys
-    graph.each do |key, value|
-      unless (value & history).empty?
-        value.zip(value & history).map! {|x, y| value.delete(x) if x == y}
+      show << keys
+      graph.each do |key, value|
+        unless (value & history).empty?
+          value.zip(value & history).map! {|x, y| value.delete(x) if x == y}
+        end
       end
-    end
 
+    end
+    puts "S с чертой внизу: #{show}"
   end
+
+  def up_s
+    graph = {}
+    sent_graph = {}
+    puts "Введите количество вершин"
+    amount_of_keys = gets.chomp.to_i
+    total_amount = (1..amount_of_keys).to_a
+    total_amount.each do |element|
+      graph[element] = []
+      sent_graph[element] = []
+    end
+    graph.each do |key, value|
+      puts "Введите вершины, которые водят в вершину #{key}"
+      graph[key] = gets.chomp.split(',').map(&:to_i)
+      sent_graph[key] = graph[key]
+    end
+    #graph = { 1 => [], 2 => [1], 3 => [1], 4 => [2,3], 5 => [3,6], 6 => [] }
+    show = []
+    i = 0
+    until graph.empty?
+      i += 1
+      break puts "Цикличность недоступна" if i > (graph.length * 4)
+      keys = []
+      iteration_keys = []
+      graph.each do |key, value|
+        unless graph.values.flatten.include? key
+          keys << key
+          iteration_keys << key
+        end
+      end
+      graph.each do |key, value|
+        iteration_keys.each do |element|
+          graph.delete(key) if key == element
+        end
+      end
+      show << keys
+    end
+    puts "S с чертой верху: #{show}"
+    down_s(sent_graph)
+  end
+
 end
 
-def up_s
-  graph = { 1 => [], 2 => [1], 3 => [1], 4 => [2,3], 5 => [3,6], 6 => [] }
-  show = []
-  i = 0
-  until graph.empty?
-    i += 1
-    break puts "Цикличность недоступна" if i > (graph.length * 4)
-    keys = []
-    iteration_keys = []
-    graph.each do |key, value|
-      unless graph.values.flatten.include? key
-        keys << key
-        iteration_keys << key
-      end
-    end
-    graph.each do |key, value|
-      iteration_keys.each do |element|
-        graph.delete(key) if key == element
-      end
-    end
-    show << keys
-  end
-end
+AssocativeScores.new.up_s
